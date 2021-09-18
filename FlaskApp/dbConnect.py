ab74871ab34fd806a8ea6872b94c9b15806a3801
root <root@ip-172-31-14-54.us-east-2.compute.internal>
2021-09-18 02:54:28 +0000

    tried new local host for db

--- FlaskApp/dbConnect.py
+++ FlaskApp/dbConnect.py
@@ -1,7 +1,7 @@
 import MySQLdb
 
 dev conncetion():
-    conn = MySQLdb.connect(host="localhost",
+    conn = MySQLdb.connect(host="ec2-18-116-12-72.us-east-2.compute.amazonaws.com",
                            user = "root",
                            passwd = "root",
                            db = "aws")
